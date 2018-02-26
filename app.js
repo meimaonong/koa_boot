22c6a1ebefae9f8ce9f38d6fc383f8a45bfbfd22
覃超成 <meimaonong@163.com>
2018-02-26 17:09:50 +0800

    模板+JSON

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const Koa = require('koa')
 const router = require('koa-router')()
 const bodyParser = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+
+const path = require('path')
+
+// 引入 koa-static
+const staticFiles = require('koa-static')
 
 const app = new Koa()
 
+// 指定 public目录为静态资源目录，用来存放 js css images 等
+app.use(staticFiles(path.resolve(__dirname, "./public")))
+
+app.use(nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'view'),// 指定视图目录
+    nunjucksConfig: {
+      trimBlocks: true // 开启转义 防Xss
+    }
+}))
+
 app.use(bodyParser())
+
 
 router.get('/', async (ctx, next) => {
     ctx.body = 'Index'
@@ -15,7 +33,9 @@ router.post('/home', async (ctx, next) => {
 })
 
 router.get('/home', async (ctx, next) => {
-    ctx.body = 'Home'
+    await ctx.render('home/home',{
+      btnName: 'GoGoGo'
+    })
 })
 
 // 调用路由中间件
